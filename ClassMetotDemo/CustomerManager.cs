b5482cc261147fd9a5255109c2432d248f63f244
Yaren Çelik <yarenn.celik9@gmail.com>
2021-03-27 23:29:27 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void ListCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer Listed!");
+            Console.WriteLine("Id: " + customer.Id);
+            Console.WriteLine("Name: " + customer.Name);
+            Console.WriteLine("Surname: " + customer.Surname);
+            Console.WriteLine("---------------");
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer Added!");
+            Console.WriteLine(customer.Id + " " + customer.Name + " " + customer.Surname);
+            Console.WriteLine("---------------");
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer Deleted!");
+            Console.WriteLine(customer.Id + " " + customer.Name + " " + customer.Surname);
+            Console.WriteLine("---------------");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 16512819;
+            customer1.Name = "Oliver";
+            customer1.Surname = "Anderson";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 36185301;
+            customer2.Name = "Harry";
+            customer2.Surname = "Jones";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 52179446;
+            customer3.Name = "Emily";
+            customer3.Surname = "Brown";
+
+            Customer customer4 = new Customer();
+            customer4.Id = 24725099;
+            customer4.Name = "James";
+            customer4.Surname = "Morton";
+
+
+            CustomerManager customerManage = new CustomerManager();
+
+            customerManage.ListCustomer(customer1);
+            customerManage.ListCustomer(customer2);
+            customerManage.ListCustomer(customer3);
+            customerManage.ListCustomer(customer4);
+            customerManage.AddCustomer(customer3);
+            customerManage.DeleteCustomer(customer1);
+        }
+    }
+}
